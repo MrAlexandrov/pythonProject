80b4870895dda86afb7c672611159d9e2e6d2ace
MrAlexandrov <maxim_0101@bk.ru>
2022-01-04 19:37:38 +0300

    Done 3 point

--- dz3.1.3.py
+++ dz3.1.3.py
@@ -19,15 +19,12 @@ l = 1 / Tc      #
 m = 1 / Ts      # интенсивность потока обслуживания
 p = l / m
 A = [1] #       1 / 0!,      p / 1!,              p^2 / 2!,                p^3 / 3!,        p^4 / 4!, ...
-P0 = [1] #      1 / 1,  1 / (1 + p),    1 / (1 + p + p^2 / 2!),  1 / (1 + p + p^2 / 2! + p^3 / 3!) ...
-# A[i] * P0[i] = P[i]
 
 sm = A[0]
 
 for i in range(1, 100):
     A.append(A[-1] * p / i)
     sm += A[-1]
-    P0.append(1 / sm)
 
 
 def sumAn(n):
@@ -40,24 +37,7 @@ def sumAn(n):
 n = 12
 k = 12
 
-# Q[n][i] = A[n] * (p / n) ^ k
-Q = [[0] * (n + 7) for i in range(n + 7)]
-P0Q = [[0] * (n + 7) for i in range(n + 7)]
-
-
-for i in range(n + 7):
-    Q[i][0] = 1
-    P0Q[i][0] = P0[i]
-
 sm = 0
-for i in range(1, n + 5):
-    sm = sumAn(i)
-    Q[i][0] = A[i]
-    for j in range(1, n + 5):
-        Q[i][j] = ((p / i) * Q[i][j - 1])
-        sm += Q[i][j]
-        P0Q[i][j] = 1 / sm
-
 
 P0INF = [1]
 for i in range(1, n + 7):
@@ -66,54 +46,18 @@ for i in range(1, n + 7):
     sm += A[i] * a / (1 - a)
     P0INF.append(1 / sm)
 
-
-def sumQk(n, k):
+def Mzan1(n):
     res = 0
-    for i in range(1, k + 1):
-        res += Q[n][i]
+    for i in range(n + 1):
+        res += i * P0INF[n] * A[i]
+    a = p / n
+    res += P0INF[n] * (n * (p ** n) / factorial(n)) * (a / (1 - a))
     return res
-
-
-# P0[n] * (A[0] + A[1] + .. + A[n]) = 1
-check = sumAn(5)
-check *= P0[5]
-print(f"check without queue work = {check}")
-
-# P(n) = A[n] * P0[n]
-check = sumAn(5)
-check *= P0Q[5][0]
-print(f"check without queue = {check}")
-
-# P(n + k) = P0Q[n][k] * Q[n][k]
-# P0Q[n][k] * (A[0] + A[1] + .. + A[n] +
-#             (Q[n][1] + Q[n][2] + .. + Q[n][k])) = 1
-# Q[n][k] = (p / n) ^ k * A[n]
-
-
-check = sumAn(5)
-check += sumQk(5, 3)
-check *= P0Q[5][3]
-print(f"check with queue: {check}")
-
-
-def check(n, k):
-    res = P0Q[n][k] * (sumAn(n) + sumQk(n, k))
-    return res
-
-
-print(f"check(n, k) = {check(5, 3)}")
 
 
 def Mzan(n):
     x = [i for i in range(6, n + 1)]
-    y = []
-    for k in x:
-        res = 0
-        for i in range(k + 1):
-            res += P0INF[k] * i * (p ** i) / factorial(i)
-        a = p / k
-        res += P0INF[k] * (k * (p ** k) / factorial(k)) * (a / (1 - a))
-        y.append(res)
+    y = [Mzan1(i) for i in x]
     plt.title(f"M зан")  # заголовок
     plt.xlabel("n")  # ось абсцисс
     plt.ylabel("M")  # ось ординат
@@ -122,87 +66,65 @@ def Mzan(n):
     plt.show()
 
 
+def Kzagr1(n):
+    return Mzan1(n) / n
+
+
 def Kzagr(n):
     x = [i for i in range(6, n + 1)]
-    y = []
-    for k in x:
-        res = 0
-        for i in range(k + 1):
-            res += P0INF[k] * i * (p ** i) / factorial(i)
-        a = p / k
-        res += P0INF[k] * (k * (p ** k) / factorial(k)) * (a / (1 - a))
-        y.append(res)
-    for i in range(1, len(y)):
-        y[i] /= i
+    y = [Kzagr1(i) for i in x]
     plt.title(f"K загр")  # заголовок
     plt.xlabel("n")  # ось абсцисс
     plt.ylabel("K")  # ось ординат
     plt.grid()  # включение отображение сетки
-    plt.plot(x, y, label=f"k={n}")
-    plt.legend()
+    plt.plot(x, y)
     plt.show()
 
 
 def sumPINF(n):
     res = P0INF[n]
-    for i in range(1, n):
+    for i in range(1, n + 1):
         res += A[i] * P0INF[n]
-    return 1 - res
+    return res
+
+
+def Pque1(n):
+    res = 1
+    for i in range(n + 1):
+        res -= P0INF[n] * A[i]
+    return res
 
 
 def Pque(n):
-    # x = [i for i in range(k + 1)]
-    # y = [sumQk(i, n) for i in x]
-    # y[0] = 1
-    # # for i in range(1, k + 1):
-    # #     y[i] += y[i - 1]
-    # for i in range(1, k + 1):
-    #     y[i] *= P0Q[i][n]
-    x = [i for i in range(1, n)]
-    y = [sumPINF(i) for i in x]
+    x = [i for i in range(6, n + 1)]
+    y = [Pque1(i) for i in x]
+    # y1 = [P0INF[i] * A[i] * (p / i) / (1 - (p / i)) for i in x]
     plt.title(f"P оч")  # заголовок
     plt.xlabel("n")  # ось абсцисс
     plt.ylabel("P")  # ось ординат
     plt.grid()  # включение отображение сетки
-    plt.plot(x, y, label=f"k={n}")
-    plt.legend()
+    plt.plot(x, y)
+    # plt.plot(x, y1, label=f"k = {n}")
     plt.show()
 
 
+def MlenQ1(n):
+    a = p / n
+    return A[n] * P0INF[n] * a / ((1 - a) ** 2)
+
+
 def MlenQ(n):
-    x = [i for i in range(k + 1)]
-    y = [Q[i][n] * P0Q[i][n] for i in x]
-    y[0] = 0
-    for i in range(1, k + 1):
-        y[i] *= i
-        y[i] += y[i - 1]
+    x = [i for i in range(6, n + 1)]
+    y = [MlenQ1(i) for i in x]
     plt.title(f"M длины очереди")  # заголовок
     plt.xlabel("n")  # ось абсцисс
     plt.ylabel("M")  # ось ординат
     plt.grid()  # включение отображение сетки
-    plt.plot(x, y, label=f"k={n}")
-    plt.legend()
+    plt.plot(x, y)
     plt.show()
-
-
-def KzanQ(n):
-    x = [i for i in range(k + 1)]
-    y = [Q[i][n] * P0Q[i][n] for i in x]
-    y[0] = 0
-    for i in range(1, k + 1):
-        y[i] *= i
-        y[i] += y[i - 1]
-    for i in range(1, k + 1):
-        y[i] /= i
-    plt.title(f"K занятости очереди")  # заголовок
-    plt.xlabel("n")  # ось абсцисс
-    plt.ylabel("M")  # ось ординат
-    plt.grid()  # включение отображение сетки
-    plt.plot(x, y, label=f"k={n}")
-    plt.legend()
 
 
 Mzan(n)
 Kzagr(n)
-
-
+Pque(n)
+MlenQ(n)
