1aba5d6052ff57e8d2d9aa7f1df53ddcc7c8fb19
MrAlexandrov <maxim_0101@bk.ru>
2022-01-04 22:42:01 +0300

    Add 4 point

--- dz3.1.4.py
+++ dz3.1.4.py
@@ -14,17 +14,17 @@ RGB = [5, 27, 28, 22]
 
 Tc = R[1]
 Ts = G[1] + B[1] + RGB[2]
+Tw = RGB[1] + RGB[2] + RGB[3]
 
 l = 1 / Tc      #
 m = 1 / Ts      # интенсивность потока обслуживания
+d = 1 / Tw
 p = l / m
 A = [1] #       1 / 0!,      p / 1!,              p^2 / 2!,                p^3 / 3!,        p^4 / 4!, ...
 
-sm = A[0]
 
-for i in range(1, 100):
+for i in range(1, 13):
     A.append(A[-1] * p / i)
-    sm += A[-1]
 
 
 def sumAn(n):
@@ -35,28 +35,43 @@ def sumAn(n):
 
 
 n = 12
-k = 12
+k = 40
 
-sm = 0
 
-P0INF = [1]
-for i in range(1, n + 7):
+def coef(n, k):
+    return l / (n * m + k * d)
+
+
+W = [[0] * k for i in range(n + 1)]
+sumW = [0 for i in range(n + 1)]
+
+for i in range(1, n + 1):
+    W[i][0] = 1
+    for j in range(1, k):
+        W[i][j] = W[i][j - 1] * coef(i, j)
+        sumW[i] += W[i][j]
+
+
+P0INFW = [1]
+for i in range(1, n + 1):
     sm = sumAn(i)
-    a = p / i
-    sm += A[i] * a / (1 - a)
-    P0INF.append(1 / sm)
+    sm += A[i] * sumW[i]
+    P0INFW.append(1 / sm)
+
+st = 1
+
 
 def Mzan1(n):
     res = 0
     for i in range(n + 1):
-        res += i * P0INF[n] * A[i]
-    a = p / n
-    res += P0INF[n] * (n * (p ** n) / factorial(n)) * (a / (1 - a))
+        res += i * A[i]
+    res += n * A[n] * sumW[n]
+    res *= P0INFW[n]
     return res
 
 
 def Mzan(n):
-    x = [i for i in range(6, n + 1)]
+    x = [i for i in range(st, n + 1)]
     y = [Mzan1(i) for i in x]
     plt.title(f"M зан")  # заголовок
     plt.xlabel("n")  # ось абсцисс
@@ -71,7 +86,7 @@ def Kzagr1(n):
 
 
 def Kzagr(n):
-    x = [i for i in range(6, n + 1)]
+    x = [i for i in range(st, n + 1)]
     y = [Kzagr1(i) for i in x]
     plt.title(f"K загр")  # заголовок
     plt.xlabel("n")  # ось абсцисс
@@ -81,22 +96,15 @@ def Kzagr(n):
     plt.show()
 
 
-def sumPINF(n):
-    res = P0INF[n]
-    for i in range(1, n + 1):
-        res += A[i] * P0INF[n]
-    return res
-
-
 def Pque1(n):
     res = 1
     for i in range(n + 1):
-        res -= P0INF[n] * A[i]
+        res -= P0INFW[n] * A[i]
     return res
 
 
 def Pque(n):
-    x = [i for i in range(6, n + 1)]
+    x = [i for i in range(st, n + 1)]
     y = [Pque1(i) for i in x]
     # y1 = [P0INFW[i] * A[i] * (p / i) / (1 - (p / i)) for i in x]
     plt.title(f"P оч")  # заголовок
@@ -109,12 +117,14 @@ def Pque(n):
 
 
 def MlenQ1(n):
-    a = p / n
-    return A[n] * P0INF[n] * a / ((1 - a) ** 2)
+    t = 0
+    for i in range(1, len(W)):
+        t += i * W[n][i]
+    return A[n] * P0INFW[n] * t
 
 
 def MlenQ(n):
-    x = [i for i in range(6, n + 1)]
+    x = [i for i in range(st, n + 1)]
     y = [MlenQ1(i) for i in x]
     plt.title(f"M длины очереди")  # заголовок
     plt.xlabel("n")  # ось абсцисс
